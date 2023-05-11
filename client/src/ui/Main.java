e7e526db78ac80b9f5257b7cfb764d65a2c14f99
zacwastaken <seamzacdisc1@gmail.com>
2023-05-11 12:33:31 -0500

    initial commit to repo

---
+++ client/src/ui/Main.java
@@ -0,0 +1,10 @@
+package client.src.ui;
+
+/** Main endpoint for the client
+ * @author Samuel Hernández
+ * @version 0.1.0
+ * @since 0.1.0
+ * */
+public class Main {
+
+}
---
+++ server/src/model/Server.java
@@ -0,0 +1,10 @@
+package server.src.model;
+
+/** Main endpoint for the server
+ * @author Samuel Hernández
+ * @version 0.1.0
+ * @since 0.1.0
+ * */
+public class Server {
+
+}
