483e2b1a1c282b5db864e38ea6251c8ae1962c2c
Laura-Martinez182 <71099708+Laura-Martinez182@users.noreply.github.com>
2023-05-31 15:41:33 -0500

    Fixed to work between more clients

--- client/src/main/java/co/edu/icesi/model/Client.java
+++ client/src/main/java/co/edu/icesi/model/Client.java
@@ -1,48 +1,47 @@
 package co.edu.icesi.model;
 
-import co.edu.icesi.ui.ClientController;
+//import co.edu.icesi.ui.ClientController;
 import javafx.scene.layout.VBox;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
-    private Thread listenerThread;
+    private String username;
 
-    public Client(Socket socket) throws IOException {
-        this.socket = socket;
-        this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
-        System.out.println("Connected to server at " + socket.getInetAddress().getHostAddress() + ":" + socket.getLocalPort());
-    }
 
-    public void receiveMessageFromServer(VBox messagesVB) {
-        System.out.println("Listening for server incoming messages...");
-        listenerThread = new Thread(() -> {
-            while (socket.isConnected()) {
-                try {
-                    String messageFromServer = bufferedReader.readLine();
-                    ClientController.addBubble(messageFromServer, messagesVB);
-                } catch (IOException ioe) {
-                    System.out.println("Error receiving message from server.");
-                    ioe.printStackTrace();
-                    break;
-                }
-            }
-        });
-
-        listenerThread.start();
-    }
-
-    public void sendMessageToServer(String messageToSend) {
+    public Client(Socket socket, String username) throws IOException {
         try {
-            bufferedWriter.write(messageToSend);
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+            this.username = username;
+            System.out.println("Connected to server at " + socket.getInetAddress().getHostAddress() + ":" + socket.getLocalPort());
+        } catch (IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessage() {
+        try {
+            bufferedWriter.write(username);
             bufferedWriter.newLine();
             bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+
         } catch (IOException ioe) {
             System.out.println("Error sending message to the server.");
             ioe.printStackTrace();
@@ -51,29 +50,115 @@ public class Client {
         System.out.println("Message sent to server");
     }
 
-    public void closeEverything (Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
-        try {
-            if (bufferedReader != null) bufferedReader.close();
-            if (bufferedWriter != null) bufferedWriter.close();
-            if (socket != null) socket.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+    public void readMessage(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msg;
+
+                while (socket.isConnected()){
+                    try {
+                        msg = bufferedReader.readLine();
+                        System.out.println(msg);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try{
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+        }catch (IOException error){
+            error.printStackTrace();
         }
     }
 
-    public Thread getListenerThread() {
-        return listenerThread;
+    public static void main(String[] args) throws IOException{
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter your username");
+       String username = scanner.nextLine();
+       Socket socket = new Socket("localhost", 1234);
+       Client client = new Client(socket,username);
+       client.readMessage();
+       client.sendMessage();
     }
 
-    public Socket getSocket() {
-        return socket;
-    }
 
-    public BufferedReader getBufferedReader() {
-        return bufferedReader;
-    }
 
-    public BufferedWriter getBufferedWriter() {
-        return bufferedWriter;
-    }
+//    private Thread listenerThread;
+//
+//    public Client(Socket socket) throws IOException {
+//        this.socket = socket;
+//        this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+//        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+//        System.out.println("Connected to server at " + socket.getInetAddress().getHostAddress() + ":" + socket.getLocalPort());
+//    }
+//
+//    public void receiveMessageFromServer(VBox messagesVB) {
+//        System.out.println("Listening for server incoming messages...");
+//        listenerThread = new Thread(() -> {
+//            while (socket.isConnected()) {
+//                try {
+//                    String messageFromServer = bufferedReader.readLine();
+//                    ClientController.addBubble(messageFromServer, messagesVB);
+//                } catch (IOException ioe) {
+//                    System.out.println("Error receiving message from server.");
+//                    ioe.printStackTrace();
+//                    break;
+//                }
+//            }
+//        });
+//
+//        listenerThread.start();
+//    }
+//
+//    public void sendMessageToServer(String messageToSend) {
+//        try {
+//            bufferedWriter.write(messageToSend);
+//            bufferedWriter.newLine();
+//            bufferedWriter.flush();
+//        } catch (IOException ioe) {
+//            System.out.println("Error sending message to the server.");
+//            ioe.printStackTrace();
+//            closeEverything(socket, bufferedReader, bufferedWriter);
+//        }
+//        System.out.println("Message sent to server");
+//    }
+//
+//    public void closeEverything (Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+//        try {
+//            if (bufferedReader != null) bufferedReader.close();
+//            if (bufferedWriter != null) bufferedWriter.close();
+//            if (socket != null) socket.close();
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+//    }
+//
+//    public Thread getListenerThread() {
+//        return listenerThread;
+//    }
+//
+//    public Socket getSocket() {
+//        return socket;
+//    }
+//
+//    public BufferedReader getBufferedReader() {
+//        return bufferedReader;
+//    }
+//
+//    public BufferedWriter getBufferedWriter() {
+//        return bufferedWriter;
+//    }
 }
--- server/src/main/java/co/edu/icesi/model/Server.java
+++ server/src/main/java/co/edu/icesi/model/Server.java
@@ -1,6 +1,6 @@
 package co.edu.icesi.model;
 
-import co.edu.icesi.ui.ServerController;
+//import co.edu.icesi.ui.ServerController;
 import javafx.scene.layout.VBox;
 
 import java.io.*;
@@ -10,78 +10,127 @@ import java.net.Socket;
 public class Server {
 
     private ServerSocket serverSocket;
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-    private Thread listenerThread;
+//    private Socket socket;
+//    private BufferedReader bufferedReader;
+//    private BufferedWriter bufferedWriter;
+//    private Thread listenerThread;
 
-    public Server(ServerSocket serverSocket) throws IOException {
+
+    public Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
-        this.socket = serverSocket.accept();
-        this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(this.socket.getOutputStream()));
-        System.out.println("Server is up and running at " + serverSocket.getInetAddress().getHostAddress() + ":" + serverSocket.getLocalPort());
     }
 
-    public void receiveMessageFromClient(VBox messagesVB) {
-        listenerThread = new Thread(() -> {
-            System.out.println("Listening for client incoming messages...");
-            while (socket.isConnected()) {
-                try {
-                    String messageFromClient = bufferedReader.readLine();
-                    ServerController.addBubble(messageFromClient, messagesVB);
-                } catch (IOException ioe) {
-                    System.out.println("Error receiving message from client.");
-                    ioe.printStackTrace();
-                    break;
-                }
+    public void startServer(){
+        try{
+            while (!serverSocket.isClosed()){
+                Socket socket = serverSocket.accept();
+                System.out.println("Client has connected");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
-        });
-
-        listenerThread.start();
-    }
-
-    public void sendMessageToClient(String messageToSend) {
-        try {
-            bufferedWriter.write(messageToSend);
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-        } catch (IOException ioe) {
-            System.out.println("Error sending message to the client.");
-            ioe.printStackTrace();
-            closeEverything(serverSocket, socket, bufferedReader, bufferedWriter);
-        }
-        System.out.println("Message sent to client");
-    }
-
-    public void closeEverything (ServerSocket serverSocket, Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
-        try {
-            if (bufferedReader != null) bufferedReader.close();
-            if (bufferedWriter != null) bufferedWriter.close();
-            if (socket != null) socket.close();
-            if (serverSocket != null) serverSocket.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (IOException error){
+            System.out.println("Error starting the server");
         }
     }
 
-    public Thread getListenerThread() {
-        return listenerThread;
+    public void closeServer(){
+        try {
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        } catch (IOException error){
+            System.out.println("Error closing the server");
+            error.printStackTrace();
+        }
     }
 
-    public ServerSocket getServerSocket() {
-        return serverSocket;
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 
-    public Socket getSocket() {
-        return socket;
-    }
 
-    public BufferedReader getBufferedReader() {
-        return bufferedReader;
-    }
 
-    public BufferedWriter getBufferedWriter() {
-        return bufferedWriter;
-    }
+//    public Server(ServerSocket serverSocket) throws IOException {
+//        this.serverSocket = serverSocket;
+//        this.socket = serverSocket.accept();
+//        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+//        System.out.println("Server is up and running at " + serverSocket.getInetAddress().getHostAddress() + ":" + serverSocket.getLocalPort());
+//    }
+//
+//    public void receiveMessageFromClient(VBox messagesVB) {
+//        listenerThread = new Thread(() -> {
+//            System.out.println("Listening for client incoming messages...");
+//            while (socket.isConnected()) {
+//                try {
+//                    String messageFromClient = bufferedReader.readLine();
+//                    ServerController.addBubble(messageFromClient, messagesVB);
+//                } catch (IOException ioe) {
+//                    System.out.println("Error receiving message from client.");
+//                    closeEverything(serverSocket, socket, bufferedReader, bufferedWriter);
+//                    ioe.printStackTrace();
+//                    break;
+//                }
+//            }
+//        });
+//
+//        listenerThread.start();
+//    }
+//
+//    public void sendMessageToClient(String messageToSend) {
+//        try {
+//            bufferedWriter.write(messageToSend);
+//            bufferedWriter.newLine();
+//            bufferedWriter.flush();
+//        } catch (IOException ioe) {
+//            System.out.println("Error sending message to the client.");
+//            ioe.printStackTrace();
+//            closeEverything(serverSocket, socket, bufferedReader, bufferedWriter);
+//        }
+//        System.out.println("Message sent to client");
+//    }
+//
+//    public void closeEverything (ServerSocket serverSocket, Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+//        try {
+//            if (bufferedReader != null){
+//                bufferedReader.close();
+//            }
+//            if (bufferedWriter != null){
+//                bufferedWriter.close();
+//            }
+//            if (socket != null){
+//                socket.close();
+//            }
+//            if (serverSocket != null) {
+//                serverSocket.close();
+//            }
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+//    }
+//
+//    public Thread getListenerThread() {
+//        return listenerThread;
+//    }
+//
+//    public ServerSocket getServerSocket() {
+//        return serverSocket;
+//    }
+//
+//    public Socket getSocket() {
+//        return socket;
+//    }
+//
+//    public BufferedReader getBufferedReader() {
+//        return bufferedReader;
+//    }
+//
+//    public BufferedWriter getBufferedWriter() {
+//        return bufferedWriter;
+//    }
 }
